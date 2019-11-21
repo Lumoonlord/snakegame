ffe3dd33e7f29ead76f4c19ec912fad65a2b6cfb
moonlord <1134774044@qq.com>
2019-11-22 02:43:44 +0800

    贪吃蛇小游戏

---
+++ src/lu/moonlord/code/Msnake.java
@@ -0,0 +1,14 @@
+package lu.moonlord.code;
+
+import javax.swing.*;
+
+public class Msnake {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setBounds(10,10,900,720);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+}
